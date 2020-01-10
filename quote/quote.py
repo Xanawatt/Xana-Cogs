d0c040a3c3bd0348070ca75466982ba889546363
Xanawatt <Schneidermark2000@gmail.com>
2020-01-09 20:44:16 -0500

    Added listquotes

--- quote/quote.py
+++ quote/quote.py
@@ -64,12 +64,17 @@ class Quote(commands.Cog):
 
 		return quote_to_add  # To appease await signal
 
-	"""
+
 	# @quote.command(name="list")
 	@commands.command(name="listquotes")
 	async def quote_list(self, ctx):
-		await ctx.maybe_send_embed(quote_list)
-	"""
+		message = ""
+		message += "Quote list: + \n + ``` + \n"
+
+		for index, quote in enumerate(self.quote_list):
+			message += str(index) + ". " + quote.replace("barlow_emote", self.barlow_emote) + "\n"
+		await self.send_message(ctx.channel, message, delay=0)
+
 
 	def get_quote(self, ctx, quote_id):
 		"""Get a random quote or supply a quote_id to get a quote"""
@@ -87,7 +92,7 @@ class Quote(commands.Cog):
 				return self.quote_list[quote_id]
 
 
-	async def send_message(self, channel: discord.TextChannel, message: str):
+	async def send_message(self, channel: discord.TextChannel, message: str, delay=0.01):
 		"""
 		Send a mwessage to a channew.
 		Wiww send a typing indicatow, and wiww wait a vawiabwe amount of time
@@ -95,7 +100,7 @@ class Quote(commands.Cog):
 		"""
 		try:
 			async with channel.typing():
-				await asyncio.sleep(len(message) * 0.01)
+				await asyncio.sleep(len(message) * delay)
 				await self.bot.send_filtered(channel, content=message)
 		except discord.Forbidden:
 			pass  # Not awwowed to send mwessages in dis channel
