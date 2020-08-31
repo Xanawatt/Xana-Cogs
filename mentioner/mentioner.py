8b56b7b149402626cba366193e11bef50f5d6888
Xanawatt <Schneidermark2000@gmail.com>
2020-08-31 14:59:22 -0400

    should fix discord not sending none when they should be

--- mentioner/mentioner.py
+++ mentioner/mentioner.py
@@ -99,7 +99,7 @@ class Mentioner(commands.Cog):
 	@mentionset.command()
 	async def remove(self, ctx, channel):
 		"""Remove a channel that was previously ignored"""
-		if ctx.message.channel_mentions is not None and ctx.guild.get_channel(channel) is None:
+		if len(ctx.message.channel_mentions) > 0 and ctx.guild.get_channel(channel) is None:
 			await self.send_message(ctx, str(ctx.message.channel_mentions))
 			await self.send_message(ctx, str(ctx.guild.get_channel(channel)))
 			for channel in ctx.message.channel_mentions:
